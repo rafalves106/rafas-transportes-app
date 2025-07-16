7e0667a5b9e6d0cdb432581e7a46d122048d3c8c
Rafael <rafalves106@icloud.com>
2025-07-16 10:07:34 -0300

    Correção backend CORS

--- backend/api/src/main/java/br/com/rafas/transportes/api/infra/security/SecurityConfigurations.java
+++ backend/api/src/main/java/br/com/rafas/transportes/api/infra/security/SecurityConfigurations.java
@@ -54,7 +54,10 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://rafas-transportes-app.vercel.app/"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
